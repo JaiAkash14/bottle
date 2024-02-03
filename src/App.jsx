4b4fabf40c1d0696a00b5c659e815815e8ad531d
JaiAkash14 <jaiakashmurugan0@gmail.com>
2024-02-04 00:27:03 +0530

    bottle completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,83 @@
-import"./App.scss"
+import { useRef, useEffect } from "react";
+import "./App.scss"
 import { Body } from "./Body/Body";
 import { Hero } from "./Hero/Hero";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform, useMotionValueEvent } from "framer-motion";
 
 
-const variants ={
-  initial:{
+const variants = {
+  initial: {
     y: 0,
   },
-  animate:{
-    y:-200,
-    transition:{
-      duration:2,
-      delay:2,
+  animate: {
+    y: -200,
+    transition: {
+      duration: 2,
+      delay: 2,
 
     }
   },
 
 
-  initialBottle:{
+  initialBottle: {
     y: 0,
   },
-  animateBottle:{
-    y:+280,
-    transition:{
-      duration:2,
-      delay:2,
+  animateBottle: {
+    y: +280,
+    transition: {
+      duration: 2,
+      delay: 2,
 
     }
   }
 }
 
 
-
 function App() {
 
+
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+ 
+  const { scrollYProgress } = useScroll(
+
+  )
+  const ybg = useTransform(scrollYProgress, [0, 0.5], ["0%", "95%"])
+  const yText = useTransform(scrollYProgress, [0, 0.5], ["0%", "-40%"])
+
+
+
+// useEffect(()=>{
+// console.log(scrollYProgress);
+// },[scrollYProgress])
+
+
   return (
+    <div
+      className="App">
+      <motion.div className="image" variants={variants} intial="initial" animate="animate">
 
-    
-    <div className="App">
- <motion.div className="image" variants={variants} intial="initial" animate="animate">
+        <motion.img style={{ y: ybg }} src="Mask group.png" alt="" />
+      </motion.div>
+      <motion.div className="imageBottle" variants={variants} intial="initialBottle" animate="animateBottle">
 
-  <img src="Mask group.png" alt="" />
- </motion.div>
- <motion.div className="imageBottle" variants={variants} intial="initialBottle" animate="animateBottle">
+        <motion.img style={{y:yText }} src="Group 2836 (1).png" alt="" />
+      </motion.div>
 
-<img src="Group 2836 (1).png" alt="" />
-</motion.div>
+      <motion.section>
+
+        <Hero />
 
 
-      <section>
-    
-        <Hero/>
-     
-      
-      </section>
-      <section><Body/></section>
-      
-      </div>
-    
+      </motion.section>
+      <motion.section>
+        <Body />
+        </motion.section>
+
+    </div>
+
   );
 }
 
--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,11 +1,23 @@
 import "./body.scss"
 
+
+
+
 export const Body = () => {
-  return (
-    <div className="body">
-        <div className="circleL"></div>
-     
-        <img src="Frame 1321314729.png"></img>
-    </div>
-  )
+    return (
+        <div className="body">
+            <div className="circleL"></div>
+            <div className="content">
+                <img src="./Vector.png"></img>
+                <h1>sustainable & bpa free</h1>
+                <img src="ion_water.png"></img>
+                <h1>lightweight</h1>
+                <img src="fontisto_recycle.png"></img>
+                <h1>lifetime warranty</h1>
+            </div>
+            <img src="Frame 1321314729.png"></img>
+
+
+        </div>
+    )
 }
